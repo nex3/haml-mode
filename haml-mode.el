nex3/haml-mode

commit 66be90d9c3a30f9dd232832d478ad46eaae903c3

Don't require a space before ruby code
As far as I can tell this space is not a requirement of Haml nor does it
impair the workings of the Haml or Ruby code.

haml-mode.el
@@ -237,7 +237,7 @@ END.")
   "Regexp to match trailing ruby code which may continue onto subsequent lines.")
 
 (defconst haml-ruby-script-re
-  (concat "^[ \t]*\\(-\\|[&!]?[=~]\\) " haml-possibly-multiline-code-re)
+  (concat "^[ \t]*\\(-\\|[&!]?[=~]\\)" haml-possibly-multiline-code-re)
   "Regexp to match -, = or ~ blocks and any continued code lines.")
 
 (defun haml-highlight-ruby-script (limit)

commit 8a18ef8bfcd1d2bd0794c54c9d8238ee11c838e3

Don't highlight '== #{foo} bar' as a comment
This fixes a regression introduced by 66be90d.

haml-mode.el
@@ -237,7 +237,7 @@ END.")
   "Regexp to match trailing ruby code which may continue onto subsequent lines.")
 
 (defconst haml-ruby-script-re
-  (concat "^[ \t]*\\(-\\|[&!]?[=~]\\)" haml-possibly-multiline-code-re)
+  (concat "^[ \t]*\\(-\\|[&!]?\\(?:=[^=]\\|~\\)\\)" haml-possibly-multiline-code-re)
   "Regexp to match -, = or ~ blocks and any continued code lines.")
 
 (defun haml-highlight-ruby-script (limit)